c222dc079927105ee98b08174ecc6984cb824eb5
ArchitectBot <61872893+architectbot@users.noreply.github.com>
2021-02-16 10:32:26 +0000

    Align files (#97)
    Co-authored-by: github-actions <action@github.com>

--- Makefile.gen.go.mk
+++ Makefile.gen.go.mk
@@ -1,9 +1,7 @@
 # DO NOT EDIT. Generated with:
 #
-#    devctl@4.2.2-dev
+#    devctl@4.3.0
 #
-
-PACKAGE_DIR    := ./bin-dist
 
 APPLICATION    := $(shell go list -m | cut -d '/' -f 3)
 BUILDTIMESTAMP := $(shell date -u '+%FT%TZ')
@@ -48,24 +46,6 @@ $(APPLICATION)-v$(VERSION)-%-amd64: $(SOURCES)
 	@echo "====> $@"
 	CGO_ENABLED=0 GOOS=$* GOARCH=amd64 go build -ldflags "$(LDFLAGS)" -o $@ .
 
-.PHONY: package-darwin package-linux
-## package-darwin: prepares a packaged darwin/amd64 version
-package-darwin: $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-darwin-amd64.tar.gz
-	@echo "====> $@"
-## package-linux: prepares a packaged linux/amd64 version
-package-linux: $(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-linux-amd64.tar.gz
-	@echo "====> $@"
-
-$(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-%-amd64.tar.gz: DIR=$(PACKAGE_DIR)/$<
-$(PACKAGE_DIR)/$(APPLICATION)-v$(VERSION)-%-amd64.tar.gz: $(APPLICATION)-v$(VERSION)-%-amd64
-	@echo "====> $@"
-	mkdir -p $(DIR)
-	cp $< $(DIR)/$(APPLICATION)
-	cp README.md LICENSE $(DIR)
-	tar -C $(PACKAGE_DIR) -cvzf $(PACKAGE_DIR)/$<.tar.gz $<
-	rm -rf $(DIR)
-	rm -rf $<
-
 .PHONY: install
 ## install: install the application
 install:
@@ -107,4 +87,5 @@ test:
 ## build-docker: builds docker image to registry
 build-docker: build-linux
 	@echo "====> $@"
+	cp -a $(APPLICATION)-linux $(APPLICATION)
 	docker build -t ${APPLICATION}:${VERSION} .
