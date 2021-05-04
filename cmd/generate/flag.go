a75ca7cc1871fa30e5c75714674932197c91a6f8
kubasobon <wtty.fool@gmail.com>
2021-05-04 14:24:28 +0200

    add --dir flag

--- cmd/generate/flag.go
+++ cmd/generate/flag.go
@@ -7,24 +7,29 @@ import (
 
 const (
 	flagApp          = "app"
+	flagDir          = "dir"
 	flagInstallation = "installation"
 	flagName         = "name"
 	flagNamespace    = "namespace"
 	flagRaw          = "raw"
 	flagVerbose      = "verbose"
+	flagVersion      = "version"
 )
 
 type flag struct {
 	App          string
+	Dir          string
 	Installation string
-	Name         string
 	Namespace    string
+	Name         string
 	Raw          bool
 	Verbose      bool
+	Version      string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.App, flagApp, "", `Name of an application to generate the config for (e.g. "kvm-operator").`)
+	cmd.Flags().StringVar(&f.Dir, flagDir, ".", `Directory containing configuration source (e.g cloned "giantswarm/config" repo).`)
 	cmd.Flags().StringVar(&f.Installation, flagInstallation, "", `Installation codename (e.g. "gauss").`)
 	cmd.Flags().StringVar(&f.Name, flagName, "giantswarm", `Name of the generated ConfigMap/Secret.`)
 	cmd.Flags().StringVar(&f.Namespace, flagNamespace, "giantswarm", `Namespace of the generated ConfigMap/Secret.`)
