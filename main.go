bc56b7ede8e7c8bf113ea812ee8bb0d6563289fd
kubasobon <wtty.fool@gmail.com>
2021-05-05 11:28:01 +0200

    remove flag package

--- main.go
+++ main.go
@@ -12,12 +12,7 @@ import (
 
 	"github.com/giantswarm/config-controller/cmd/generate"
 	"github.com/giantswarm/config-controller/cmd/lint"
-	"github.com/giantswarm/config-controller/flag"
 	"github.com/giantswarm/config-controller/pkg/project"
-)
-
-var (
-	f *flag.Flag = flag.New()
 )
 
 func main() {
@@ -84,20 +79,6 @@ func mainE(ctx context.Context) error {
 	}
 
 	newCommand.CobraCommand().AddCommand(subcommands...)
-
-	daemonCommand := newCommand.DaemonCommand().CobraCommand()
-
-	daemonCommand.PersistentFlags().Bool(f.Service.App.Unique, false, "Whether the operator is deployed as a unique app.")
-	daemonCommand.PersistentFlags().String(f.Service.Installation.Name, "", `Installation codename (e.g. "geckon")`)
-	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.Address, "http://127.0.0.1:6443", "Address used to connect to Kubernetes. When empty in-cluster config is created.")
-	daemonCommand.PersistentFlags().Bool(f.Service.Kubernetes.InCluster, false, "Whether to use the in-cluster config to authenticate with Kubernetes.")
-	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.KubeConfig, "", "KubeConfig used to connect to Kubernetes. When empty other settings are used.")
-	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CAFile, "", "Certificate authority file path to use to authenticate with Kubernetes.")
-	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CrtFile, "", "Certificate file path to use to authenticate with Kubernetes.")
-	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.KeyFile, "", "Key file path to use to authenticate with Kubernetes.")
-
-	newCommand.CobraCommand().SilenceErrors = true
-	newCommand.CobraCommand().SilenceUsage = true
 
 	err = newCommand.CobraCommand().Execute()
 	if err != nil {
