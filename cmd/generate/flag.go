87d04a7a2e6746fffbbfc20ed6733330bf00f52a
kubasobon <wtty.fool@gmail.com>
2021-05-07 10:45:24 +0200

    introduce new flags

--- cmd/generate/flag.go
+++ cmd/generate/flag.go
@@ -6,27 +6,38 @@ import (
 )
 
 const (
-	flagApp          = "app"
-	flagDir          = "dir"
-	flagInstallation = "installation"
-	flagName         = "name"
-	flagNamespace    = "namespace"
-	flagRaw          = "raw"
-	flagVerbose      = "verbose"
+	flagApp        = "app" // TODO: app-name
+	flagAppCatalog = "app-catalog"
+	// flagAppCRName              = "appcr-name" // TODO: is this even needed
+	flagAppDisableForceUpgrade = "app-disable-force-upgrade"
+	flagAppVersion             = "app-version"
+	flagDir                    = "dir"
+	flagInstallation           = "installation"
+	flagName                   = "name" // TODO: config-name
+	flagNamespace              = "namespace"
+	flagRaw                    = "raw"
+	flagVerbose                = "verbose"
 )
 
 type flag struct {
-	App          string
-	Dir          string
-	Installation string
-	Namespace    string
-	Name         string
-	Raw          bool
-	Verbose      bool
+	App        string
+	AppCatalog string
+	// AppCRName              string
+	AppDisableForceUpgrade bool
+	AppVersion             string
+	Dir                    string
+	Installation           string
+	Namespace              string
+	Name                   string
+	Raw                    bool
+	Verbose                bool
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.App, flagApp, "", `Name of an application to generate the config for (e.g. "kvm-operator").`)
+	cmd.Flags().StringVar(&f.AppCatalog, flagAppCatalog, "", `Name of application catalog, e.g. "control-plane-test-catalog".`)
+	cmd.Flags().BoolVar(&f.AppDisableForceUpgrade, flagAppDisableForceUpgrade, false, `Sets "chart-operator.giantswarm.io/force-helm-upgrade" flag.`)
+	cmd.Flags().StringVar(&f.AppVersion, flagAppVersion, "", `Application version to be set in App CR.`)
 	cmd.Flags().StringVar(&f.Dir, flagDir, ".", `Directory containing configuration source (e.g cloned "giantswarm/config" repo).`)
 	cmd.Flags().StringVar(&f.Installation, flagInstallation, "", `Installation codename (e.g. "gauss").`)
 	cmd.Flags().StringVar(&f.Name, flagName, "giantswarm", `Name of the generated ConfigMap/Secret.`)
@@ -38,6 +49,15 @@ func (f *flag) Init(cmd *cobra.Command) {
 func (f *flag) Validate() error {
 	if f.App == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagApp)
+	}
+	if f.AppCatalog == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagAppCatalog)
+	}
+	if f.AppVersion == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagAppVersion)
+	}
+	if f.Dir == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagDir)
 	}
 	if f.Installation == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagInstallation)
