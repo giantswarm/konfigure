67a716f9b252d63b461548974be59b801ee50030
GitHub Action <action@github.com>
2021-05-28 11:14:10 +0000

    Bump version to 0.3.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "konfigure"
 	source      = "https://github.com/giantswarm/konfigure"
-	version     = "0.3.2"
+	version     = "0.3.3-dev"
 )
 
 func Description() string {
