8eb602066584f3db3d2930a901613cfce3743eff
vol <me@volodymyrstoiko.com>
2021-08-25 17:03:40 +0300

    Add argo finalizer to enable cascading deletion

--- pkg/generator/key/key.go
+++ pkg/generator/key/key.go
@@ -5,4 +5,5 @@ const (
 	KubernetesManagedByLabel   = "app.kubernetes.io/managed-by"
 	ReleaseNameAnnotation      = "meta.helm.sh/release-name"
 	ReleaseNamespaceAnnotation = "meta.helm.sh/release-namespace"
+	ArgoResourceFinalizer      = "resources-finalizer.argocd.argoproj.io"
 )
