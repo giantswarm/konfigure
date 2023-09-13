100098b432f7dd0e85a6f7ca620bec58f1c4cbda
taylorbot <dev@giantswarm.io>
2023-09-13 11:14:57 +0000

    Release v0.15.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "konfigure"
 	source      = "https://github.com/giantswarm/konfigure"
-	version     = "0.14.5-dev"
+	version     = "0.15.0"
 )
 
 func Description() string {
