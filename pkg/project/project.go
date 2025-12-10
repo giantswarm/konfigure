71d9c22e8f86cd5c0380edc3bf38fa5fdc8e26ed
taylorbot <dev@giantswarm.io>
2025-12-10 08:29:59 +0000

    Bump version to 2.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "konfigure"
 	source      = "https://github.com/giantswarm/konfigure"
-	version     = "2.1.1"
+	version     = "2.1.2-dev"
 )
 
 func Description() string {
